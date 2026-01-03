ec463e5412f40951472a95b169b2d24e8baea635
Justin Holsgrove <jholsgrove@hotmail.co.uk>
2026-01-03 14:40:41 +0000

    make cli args/params

--- Module 1/pipeline/ingest_data.py
+++ Module 1/pipeline/ingest_data.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 # coding: utf-8
 import pandas as pd
+import click
 from sqlalchemy import create_engine
 from tqdm.auto import tqdm
+
 
 dtype = {
     "VendorID": "Int64",
@@ -28,45 +30,61 @@ parse_dates = [
     "tpep_dropoff_datetime"
 ]
 
-#df = pd.read_csv(
-#    url,
-#    nrows=100,
-#    dtype=dtype,
-#    parse_dates=parse_dates
-#)
-
-#df.head()
-
-def run():
-    pg_user = 'root'
-    pg_password = 'root'
-    pg_host = 'localhost'
-    pg_port = 5432
-    pg_db = 'ny_taxi'
-    year = 2021
-    month = 1
-    chunk_size = 100000
-    target_table = 'yellow_taxi_data'
-
+@click.command()
+@click.option('--pg-user', default='root')
+@click.option('--pg-pass', default='root')
+@click.option('--pg-host', default='localhost')
+@click.option('--pg-port', default=5432, type=int)
+@click.option('--pg-db', default='ny_taxi')
+@click.option('--year', default=2021, type=int)
+@click.option('--month', default=1, type=int)
+@click.option('--chunksize', default=100000, type=int)
+@click.option('--target-table', default='yellow_taxi_data')
+def run(pg_user, pg_pass, pg_host, pg_port, pg_db, year, month, chunksize, target_table):
     prefix = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow'
     url = f'{prefix}/yellow_tripdata_{year}-{month:02d}.csv.gz'
 
-    engine = create_engine(f'postgresql://{pg_user}:{pg_password}@{pg_host}:{pg_port}/{pg_db}')
+    engine = create_engine(
+        f'postgresql://{pg_user}:{pg_pass}@{pg_host}:{pg_port}/{pg_db}'
+    )
 
     df_iter = pd.read_csv(
         url,
         dtype=dtype,
         parse_dates=parse_dates,
         iterator=True,
-        chunksize=chunk_size,
+        chunksize=chunksize,
     )
 
     first = True
     for df_chunk in tqdm(df_iter):
-      if first:
-        df_chunk.head(0).to_sql(name=target_table, con=engine, if_exists='replace')
-        first = False
-    df_chunk.to_sql(name=target_table, con=engine, if_exists='append')
+        if first:
+            df_chunk.head(0).to_sql(
+                name=target_table,
+                con=engine,
+                if_exists='replace'
+            )
+            first = False
+
+        df_chunk.to_sql(
+            name=target_table,
+            con=engine,
+            if_exists='append'
+        )
 
 if __name__ == '__main__':
     run()
+
+#cmd
+'''
+uv run python ingest_data.py \
+  --pg-user=root \
+  --pg-pass=root \
+  --pg-host=localhost \
+  --pg-port=5432 \
+  --pg-db=ny_taxi \
+  --target-table=yellow_taxi_trips \
+  --year=2021 \
+  --month=1 \
+  --chunksize=100000
+  '''
