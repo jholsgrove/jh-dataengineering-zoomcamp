ab9f4fa00ba3e256f1a1c6f2d3bfd9d1c922f94f
Justin Holsgrove <jholsgrove@hotmail.co.uk>
2026-01-03 19:49:31 +0000

    Ingest zones pipeline

---
+++ Module 1/pipeline/ingest_zones.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+# coding: utf-8
+import pandas as pd
+import click
+from sqlalchemy import create_engine
+from tqdm.auto import tqdm
+
+dtype = {
+    "LocationID": "BIGINT",
+    "Borough": "VARCHAR(50)",
+    "Zone": "VARCHAR(50)",
+    "service_zone": "VARCHAR(50)"
+}
+
+@click.command()
+@click.option('--pg-user', default='root')
+@click.option('--pg-pass', default='root')
+@click.option('--pg-host', default='localhost')
+@click.option('--pg-port', default=5432, type=int)
+@click.option('--pg-db', default='ny_taxi')
+@click.option('--chunksize', default=100000, type=int)
+@click.option('--target-table', default='zones')
+def run(pg_user, pg_pass, pg_host, pg_port, pg_db, year, month, chunksize, target_table):
+    url = f'https://d37ci6vzurychx.cloudfront.net/misc/taxi_zone_lookup.csv'
+
+    engine = create_engine(
+        f'postgresql://{pg_user}:{pg_pass}@{pg_host}:{pg_port}/{pg_db}'
+    )
+
+    df_iter = pd.read_csv(
+        url,
+        dtype=dtype,
+        iterator=True,
+        chunksize=chunksize,
+    )
+
+    first = True
+    for df_chunk in tqdm(df_iter):
+        if first:
+            df_chunk.head(0).to_sql(
+                name=target_table,
+                con=engine,
+                if_exists='replace'
+            )
+            first = False
+
+        df_chunk.to_sql(
+            name=target_table,
+            con=engine,
+            if_exists='append'
+        )
+
+if __name__ == '__main__':
+    run()
+
+#cmd
+'''
+uv run python ingest_zones.py \
+  --pg-user=root \
+  --pg-pass=root \
+  --pg-host=localhost \
+  --pg-port=5432 \
+  --pg-db=ny_taxi \
+  --target-table=zones \
+  --chunksize=100000
+  '''
+
+'''
+docker run --rm -it \
+  --network=pipeline_default \
+  taxi_ingest:v001 \
+  --pg-user=root \
+  --pg-pass=root \
+  --pg-host=pgdatabase \
+  --pg-port=5432 \
+  --pg-db=ny_taxi \
+  --target-table=zones \
+  --chunksize=100000
+  '''
