1ba774318a42767896278592f41437e821d466bd
Justin Holsgrove <jholsgrove@hotmail.co.uk>
2026-01-04 11:25:49 +0000

    Update statements

--- Module 1/pipeline/sql/sqlStatements.sql
+++ Module 1/pipeline/sql/sqlStatements.sql
@@ -15,3 +15,40 @@ WHERE
 	t."PULocationID" = zpu."LocationID" AND
 	t."DOLocationID" = zdo."LocationID"
 LIMIT 100
+
+--USE JOIN
+
+select 
+
+t.tpep_pickup_datetime,
+t.tpep_dropoff_datetime,
+t.total_amount,
+CONCAT(zpu."Borough", ' / ', zpu."Zone") AS "pick_up_loc",
+CONCAT(zdo."Borough", ' / ', zdo."Zone") AS "drop_off_loc"
+
+from
+	yellow_taxi_trips t JOIN zones zpu 
+		ON t."PULocationID" = zpu."LocationID" 
+	JOIN zones zdo
+		ON t."DOLocationID" = zdo."LocationID"
+LIMIT 100
+
+-- LEFT JOIN look for missing DO Location Ids
+
+select 
+
+t.tpep_pickup_datetime,
+t.tpep_dropoff_datetime,
+t.total_amount,
+CONCAT(zpu."Borough", ' / ', zpu."Zone") AS "pick_up_loc",
+CONCAT(zdo."Borough", ' / ', zdo."Zone") AS "drop_off_loc"
+
+from
+	yellow_taxi_trips t JOIN zones zpu 
+		ON t."PULocationID" = zpu."LocationID" 
+	JOIN zones zdo
+		ON t."DOLocationID" = zdo."LocationID"
+
+WHERE
+	"DOLocationID" NOT IN (SELECT "LocationID" from zones)
+LIMIT 100
